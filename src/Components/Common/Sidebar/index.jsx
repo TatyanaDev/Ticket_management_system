83f535e825bcc36313243fb9e1578ffb3a6795ea
karpenko-dev <tetiana.karpenko@rezet.io>
2021-12-21 10:40:42 +0200

    Fix Sidebar

--- src/Components/Common/Sidebar/index.jsx
+++ src/Components/Common/Sidebar/index.jsx
@@ -11,36 +11,34 @@ export default function Sidebar() {
 
   return (
     <Row>
-      <Col sm={2} md={2} lg={2} xl={2} xxl={2} className={style.wrapper_sidebar}>
-        <Container className={cn(style.container_sidebar, ` ${minimize && style.container_sidebar_min}`)}>
-          <Nav defaultActiveKey='/dashboard' className='flex-column'>
-            <NavItem content='Dashboard' icon='bx:bx-home' href='/dashboard' minimize={minimize} padding={style.dash_sett_link} />
+      <Container className={cn(style.container_sidebar, ` ${minimize && style.container_sidebar_min}`)}>
+        <Nav defaultActiveKey='/dashboard' className='flex-column'>
+          <NavItem content='Dashboard' icon='bx:bx-home' href='/dashboard' minimize={minimize} padding={style.dash_sett_link} />
 
-            {minimize || <Row className={style.section_title}>Ticket</Row>}
+          {minimize || <Row className={style.section_title}>Ticket</Row>}
 
-            <NavItem content='Ticket History' icon='icon-park-outline:log' href='/ticket_history' minimize={minimize} padding={style.item_link} />
+          <NavItem content='Ticket History' icon='icon-park-outline:log' href='/ticket_history' minimize={minimize} padding={style.item_link} />
 
-            <NavItem content='Ticket List' icon='bx:bx-book' href='/ticket_list' minimize={minimize} padding={style.item_link} />
+          <NavItem content='Ticket List' icon='bx:bx-book' href='/ticket_list' minimize={minimize} padding={style.item_link} />
 
-            <NavItem content='Recent Activity' icon='feather:activity' href='/recent_activity' minimize={minimize} padding={style.item_link} />
+          <NavItem content='Recent Activity' icon='feather:activity' href='/recent_activity' minimize={minimize} padding={style.item_link} />
 
-            {minimize || <Row className={style.section_title}>Users</Row>}
+          {minimize || <Row className={style.section_title}>Users</Row>}
 
-            <NavItem content='Users List' icon='uil:users-alt' href='/user_list' minimize={minimize} padding={style.user_list_link} font={user_list} />
+          <NavItem content='Users List' icon='uil:users-alt' href='/user_list' minimize={minimize} padding={style.user_list_link} font={user_list} />
+        </Nav>
+
+        <Container className={style.wrapper}>
+          <Nav defaultActiveKey='/settings' className='flex-column'>
+            <NavItem content='Settings' icon='ic:baseline-settings' href='/settings' minimize={minimize} padding={style.dash_sett_link} />
           </Nav>
 
-          <Container className={style.wrapper}>
-            <Nav defaultActiveKey='/settings' className='flex-column'>
-              <NavItem content='Settings' icon='ic:baseline-settings' href='/settings' minimize={minimize} padding={style.dash_sett_link} />
-            </Nav>
-
-            <Col className={style.toggle} onClick={() => setMinimize(!minimize)}>
-              <Icon icon='fluent:caret-left-24-filled' className={cn(style.icon, ` ${minimize && style.icon_rotate}`)} />
-              {minimize || "Minimize"}
-            </Col>
-          </Container>
+          <Col className={style.toggle} onClick={() => setMinimize(!minimize)}>
+            <Icon icon='fluent:caret-left-24-filled' className={cn(style.icon, ` ${minimize && style.icon_rotate}`)} />
+            {minimize || "Minimize"}
+          </Col>
         </Container>
-      </Col>
+      </Container>
     </Row>
   );
 }
