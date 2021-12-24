ae31f85aae4fed839ce04a59abf04cb9250776a2
karpenko-dev <tetiana.karpenko@rezet.io>
2021-12-24 16:49:14 +0200

    Refactor dashboard page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,12 +2,13 @@ import { Container, Row, Col, Button, Table, Image } from "react-bootstrap";
 import { Icon } from "@iconify/react/dist/iconify";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { sortTicketId, sortTicketName, sortApprover, sortResolvedDate } from "./ sortingFunctions";
-import CreateNewTicketModal from "../../Components/Dashboard/CreateNewTicketModal";
+import { sortTicketId, sortTicketName, sortApprover, sortResolvedDate } from "../sortingFunctions";
+import TableContentActionRequired from "../../Components/Dashboard/TableContentActionRequired";
+import { ticketSummaryCard, tickets, recentActivityTickets } from "./mokeDB";
+import ModalOpenButton from "../../Components/Dashboard/ModalOpenButton";
 import SummaryCard from "../../Components/Dashboard/SummaryCard";
 import progress from "../../icons/dashboard/progress.svg";
 import Sidebar from "./../../Components/Shared/Sidebar";
-import { ticketSummaryCard, tickets } from "./mokeDB";
 import Header from "../../Components/Shared/Header";
 import check from "../../icons/dashboard/check.svg";
 import info from "../../icons/dashboard/info.svg";
@@ -16,9 +17,10 @@ import cn from "classnames";
 
 export default function Dashboard() {
   const [recentActivityShow, setRecentActivityShow] = useState(false);
-  const [modalShow, setModalShow] = useState(false);
+  const [currentFilter, setCurrentFilter] = useState(null);
   const [flag, setFlag] = useState(false);
 
+  const { data: recentActivity } = recentActivityTickets;
   const { data: summary } = ticketSummaryCard;
   const { data } = tickets;
 
@@ -26,19 +28,17 @@ export default function Dashboard() {
     <Container fluid>
       <Header activePageTitle='Dashboard' />
 
-      <Row className={style.test}>
+      <Row>
         <Sidebar />
+        {/* 
+        <Col className={style.text}> */}
 
         <Col className={style.container_dashboard_content}>
-          <Container className={style.wrapper_dashboard} fluid>
+          <Container className={cn(style.wrapper_dashboard, recentActivityShow && style.wrapper_dashboard_active)} fluid>
             <Row>
               <Col className={style.ticket_summary}>Ticket Summary</Col>
               <Col className={style.wrapper_button_create_ticket}>
-                <Button variant='primary' onClick={() => setModalShow(true)} className={style.button_create_ticket}>
-                  <Icon icon='gridicons:plus-small' className={style.icon_plus} />
-                  Create New Ticket
-                </Button>
-                <CreateNewTicketModal show={modalShow} onHide={() => setModalShow(false)} />
+                <ModalOpenButton content='Create New Ticket' styleButton={style.button_create_ticket} styleIcon={style.icon_plus} />
               </Col>
             </Row>
 
@@ -61,6 +61,7 @@ export default function Dashboard() {
                 </Link>
               </Col>
             </Row>
+
             <Row>
               <Col>
                 <Table className={style.wrapper_table}>
@@ -69,7 +70,7 @@ export default function Dashboard() {
                       <th className={cn(style.wrapper_thead_item, style.width_1)}>
                         <Row>
                           <Col>Ticket ID</Col>
-                          <Col className={cn(style.arrow_thead, flag && style.arrow_thead_active)} onClick={() => sortTicketId({ data, flag, setFlag })}>
+                          <Col className={cn(style.arrow_thead, currentFilter === "ticketId" && style.arrow_thead_active)} onClick={() => sortTicketId({ data, flag, setFlag, setCurrentFilter })}>
                             <Icon icon='ep:arrow-up-bold' />
                           </Col>
                         </Row>
@@ -77,7 +78,7 @@ export default function Dashboard() {
                       <th className={cn(style.wrapper_thead_item, style.width_2)}>
                         <Row>
                           <Col>Ticket Name</Col>
-                          <Col className={cn(style.arrow_thead, flag && style.arrow_thead_active)} onClick={() => sortTicketName({ data, flag, setFlag })}>
+                          <Col className={cn(style.arrow_thead, currentFilter === "ticketName" && style.arrow_thead_active)} id='ticketName' onClick={() => sortTicketName({ data, flag, setFlag, setCurrentFilter })}>
                             <Icon icon='ep:arrow-up-bold' />
                           </Col>
                         </Row>
@@ -85,7 +86,7 @@ export default function Dashboard() {
                       <th className={cn(style.wrapper_thead_item, style.width_3)}>
                         <Row>
                           <Col>Approver</Col>
-                          <Col className={cn(style.arrow_thead, flag && style.arrow_thead_active)} onClick={() => sortApprover({ data, flag, setFlag })}>
+                          <Col className={cn(style.arrow_thead, currentFilter === "approver" && style.arrow_thead_active)} id='approver' onClick={() => sortApprover({ data, flag, setFlag, setCurrentFilter })}>
                             <Icon icon='ep:arrow-up-bold' />
                           </Col>
                         </Row>
@@ -93,7 +94,7 @@ export default function Dashboard() {
                       <th className={cn(style.wrapper_thead_item, style.width_3)}>
                         <Row>
                           <Col>Resolved date</Col>
-                          <Col className={cn(style.arrow_thead, flag && style.arrow_thead_active)} onClick={() => sortResolvedDate({ data, flag, setFlag })}>
+                          <Col className={cn(style.arrow_thead, currentFilter === "resolvedDate" && style.arrow_thead_active)} id='resolvedDate' onClick={() => sortResolvedDate({ data, flag, setFlag, setCurrentFilter })}>
                             <Icon icon='ep:arrow-up-bold' />
                           </Col>
                         </Row>
@@ -103,46 +104,7 @@ export default function Dashboard() {
                   </thead>
                   <tbody>
                     {data.map((ticket) => (
-                      <>
-                        <tr key={ticket.ticket_id} className={style.wrapper_tbody}>
-                          <td className={cn(style.wrapper_tbody_item, style.content)}>{ticket.ticket_id}</td>
-                          <td className={cn(style.wrapper_tbody_item, style.gray)}>{ticket.ticket_name}</td>
-                          <td className={cn(style.wrapper_tbody_item, style.gray)}>
-                            <Row className={style.wrapper_tbody_image}>
-                              <Col className={style.tbody_image}>
-                                <Image src={ticket.src} />
-                              </Col>
-                              <Col className={style.wrapper_tbody_approver}>{ticket.approver}</Col>
-                            </Row>
-                          </td>
-                          <td className={cn(style.wrapper_tbody_item, style.content)}>{ticket.resolved_date}</td>
-                          <td className={style.wrapper_arrow_tbody}>
-                            <Icon className={style.arrow_tbody} icon='ep:arrow-down-bold' />
-                          </td>
-                        </tr>
-
-                        <tr className={style.wrapper_thead_inner}>
-                          <th className={style.wrapper_thead_item_inner}>Ticket description</th>
-                          <th></th>
-                          <th className={cn(style.wrapper_thead_item_inner, style.text_end)}>Submitted date</th>
-                          <th className={cn(style.wrapper_thead_item_inner, style.text_end)}>Followup Ticket</th>
-                          <th></th>
-                        </tr>
-
-                        <tr className={style.wrapper_tbody_inner}>
-                          <td colSpan='2' className={style.wrapper_tbody_item_inner}>
-                            {ticket.ticket_description}
-                          </td>
-                          <td className={cn(style.wrapper_tbody_item_inner, style.text_end)}>{ticket.submitted_date}</td>
-                          <td className={cn(style.wrapper_tbody_item_inner, style.text_end)}>
-                            <Button onClick={() => setModalShow(true)} className={style.button_add_ticket}>
-                              <Icon icon='gridicons:plus-small' className={style.icon_plus_add} />
-                              Add ticket
-                            </Button>
-                          </td>
-                          <td></td>
-                        </tr>
-                      </>
+                      <TableContentActionRequired ticket={ticket} key={ticket.ticket_id} />
                     ))}
                   </tbody>
                 </Table>
@@ -151,10 +113,10 @@ export default function Dashboard() {
 
             <Row>
               <Col className={style.wrapper_button_recent_activity}>
-                <Button className={style.button_recent_activity} variant='danger'>
+                <Button className={cn(style.button_recent_activity, recentActivityShow && style.button_recent_activity_active)} variant='danger' onClick={() => setRecentActivityShow(true)}>
                   <Row>
                     <Col className={style.wrapper_arrow_button}>
-                      <Icon icon='fluent:arrow-right-12-filled' rotate='270deg' className={style.arrow_button} />
+                      <Icon icon='fluent:arrow-up-12-filled' className={style.arrow_button} />
                     </Col>
                     <Col className={style.recent_activity}>Recent activity</Col>
                     <Col className={style.value_recent_activity}>{summary.action_required}</Col>
@@ -165,18 +127,52 @@ export default function Dashboard() {
           </Container>
         </Col>
 
-        <Col className={style.recent_activity}>
+        {/* <div className={style.rc}> */}
+
+        <Col className={cn(style.recent_activity_block, recentActivityShow && style.recent_activity_block_active, style.rc)}>
           <Row>
-            <Col></Col>
-            <Col> Recent activity</Col>
-            <Col>
-              <Link className={style.link_ticket_list} to='/ticket_list'>
+            <Col className={style.button_recent_activity_block} onClick={() => setRecentActivityShow(false)}>
+              <Icon icon='fluent:arrow-down-12-filled' className={style.arrow_button_block} />
+              Recent Activity
+            </Col>
+            <Col className={style.wrapper_link_ticket_list_block}>
+              <Link className={style.link_ticket_list} to='/recent_activity'>
                 See all
-                <Icon icon='ep:arrow-right-bold' className={style.arrow_see_all} />
+                <Icon icon='ep:arrow-right-bold' className={style.arrow_see_all_block} />
               </Link>
             </Col>
           </Row>
+
+          {recentActivity.map((ticket) => (
+            <Container key={ticket.ticket_id} className={cn(style.wrapper_recent_activity_ticket, ticket.type === "Take Action" && style.border_take_action)}>
+              <Row>
+                <Col className={style.icon_recent_activity_ticket}>
+                  <Image src={ticket.src} />
+                </Col>
+                <Col>
+                  <Row className={cn(style.wrapper_recent_activity_ticket_name, ticket.type === "Take Action" ? style.design_ticket_color_ticket_name : style.take_action_color_ticket_name)}>{ticket.ticket_name}</Row>
+                  <Row className={cn(style.wrapper_recent_activity_ticket_type, ticket.type === "Design ticket" ? style.background_design_ticket : style.background_take_action)}>{ticket.type}</Row>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col className={style.content_recent_activity_ticket}>{ticket.type === "Design ticket" ? `${ticket.user_name} submitted a new ticket "${ticket.ticket_name}".` : `${ticket.user_name} asked a new question: "${ticket.question}" on ${ticket.on} ticket.`}</Col>
+              </Row>
+              <Row>
+                <Col className={style.date_recent_activity_ticket}>{ticket.submitted_date}</Col>
+                <Col className={style.wrapper_review_link}>
+                  <Link className={cn(style.link_ticket_list, style.review_link, ticket.type === "Design ticket" ? style.color_reviev_link_design_ticket : style.color_reviev_link_take_action)} to='#'>
+                    Review
+                    <Icon icon='ep:arrow-right-bold' className={style.arrow_see_all_block} />
+                  </Link>
+                </Col>
+              </Row>
+            </Container>
+          ))}
         </Col>
+
+        {/* </div> */}
+        {/* </Col> */}
       </Row>
     </Container>
   );
