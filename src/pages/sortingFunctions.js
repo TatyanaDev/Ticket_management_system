abf9a342170953c50ddc3ee80b0bd0d578849d7b
karpenko-dev <tetiana.karpenko@rezet.io>
2021-12-24 16:48:57 +0200

    Fix sortingFunctions

--- src/pages/sortingFunctions.js
+++ src/pages/sortingFunctions.js
@@ -1,31 +1,35 @@
-export const sortTicketId = ({ data, flag, setFlag }) => {
+export const sortTicketId = ({ data, flag, setFlag, setCurrentFilter }) => {
   data.sort((a, b) => [
     flag ? a.ticket_id - b.ticket_id : b.ticket_id - a.ticket_id
   ]);
+  setCurrentFilter('ticketId')
   setFlag(flag => !flag);
 };
 
-export const sortTicketName = ({ data, flag, setFlag }) => {
+export const sortTicketName = ({ data, flag, setFlag, setCurrentFilter }) => {
   data.sort((a, b) => [
     flag
       ? a.ticket_name.localeCompare(b.ticket_name)
       : b.ticket_name.localeCompare(a.ticket_name),
   ]);
+  setCurrentFilter('ticketName')
   setFlag(flag => !flag);
 };
 
-export const sortApprover = ({ data, flag, setFlag }) => {
+export const sortApprover = ({ data, flag, setFlag, setCurrentFilter }) => {
   data.sort((a, b) => [
     flag
       ? a.approver.localeCompare(b.approver)
       : b.approver.localeCompare(a.approver),
   ]);
+  setCurrentFilter('approver')
   setFlag(flag => !flag);
 };
 
-export const sortResolvedDate = ({ data, flag, setFlag }) => {
+export const sortResolvedDate = ({ data, flag, setFlag, setCurrentFilter }) => {
   data.sort((a, b) => [
     flag ? new Date(a.resolved_date) - new Date(b.resolved_date) : new Date(b.resolved_date) - new Date(a.resolved_date)
   ]);
+  setCurrentFilter('resolvedDate')
   setFlag(flag => !flag);
 };
