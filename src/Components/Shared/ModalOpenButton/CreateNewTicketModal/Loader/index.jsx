baf252deb7577b3732e24d89c860e59c061c209c
karpenko-dev <tetiana.karpenko@rezet.io>
2022-01-12 10:53:20 +0200

    Added component for downloading tickets

---
+++ src/Components/Shared/ModalOpenButton/CreateNewTicketModal/Loader/index.jsx
@@ -0,0 +1,52 @@
+import { Row, Col, Form, Button, Container, Image } from "react-bootstrap";
+import { Icon } from "@iconify/react/dist/iconify";
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import cn from "classnames";
+import uploadIcon from "../../../../../icon/Upload.svg";
+import style from "./styles.module.css";
+
+export default function Loader() {
+  const [files, setFiles] = useState([]);
+  const handleDrop = (acceptedFiles) => setFiles(acceptedFiles.map((file) => ({ fileName: file.name, fileSize: `${Math.trunc(file.size / 1000)}KB` })));
+  const removeFile = ({ target }) => setFiles(files.filter((file) => file.fileName !== target.id));
+  
+  return (
+    <Form style={{ maxWidth: 531 }} method='post' encType='multipart/form-data' action=''>
+      <Dropzone onDrop={handleDrop} accept='.pdf'>
+        {({ getRootProps, getInputProps }) => (
+          <Container {...getRootProps({ className: style.dropzone })}>
+            <Form.Group>
+              <Form.Control {...getInputProps()} />
+              <Image src={uploadIcon} className={style.add_file} />
+              <Button clickable='#dropzonePreview' className={cn(style.add_file, style.button_add_file)}>
+                Add file
+              </Button>
+              <Form.Label className={style.label}>or drop files to upload</Form.Label>
+            </Form.Group>
+          </Container>
+        )}
+      </Dropzone>
+      <Row style={{ maxWidth: 419, overflow: "hidden", margin: "0 auto" }}>
+        {files.map((fileItem, i) => (
+          <Col key={i} className={style.container_file}>
+            <Row>
+              <Col className={style.wrapper_file_name}>
+                <Icon icon='ant-design:file-pdf-outlined' style={{ fontSize: 40 }} />
+              </Col>
+              <Col>
+                <Row className={style.file_name}>
+                  {fileItem.fileName}
+                  <span className={style.button_remove_file} id={fileItem.fileName} onClick={removeFile}>
+                    &#215;
+                  </span>
+                </Row>
+                <Row className={style.file_size}>{fileItem.fileSize}</Row>
+              </Col>
+            </Row>
+          </Col>
+        ))}
+      </Row>
+    </Form>
+  );
+}
--- src/Components/Shared/ModalOpenButton/CreateNewTicketModal/index.jsx
+++ src/Components/Shared/ModalOpenButton/CreateNewTicketModal/index.jsx
@@ -1,18 +1,10 @@
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+import Loader from "./Loader";
 
 export default function CreateNewTicketModal(props) {
   return (
     <Modal {...props} aria-labelledby='contained-modal-title-vcenter' centered>
-      <Modal.Header closeButton>
-        <Modal.Title id='contained-modal-title-vcenter'>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>CANCEL</Button>
-      </Modal.Footer>
+      <Loader />
     </Modal>
   );
 }
