1281c39c1056f73c29447bd3fd0957aa4fdf7128
karpenko-dev <tetiana.karpenko@rezet.io>
2022-01-13 09:31:27 +0200

    installed libraries for Text Editor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
-import './index.css';
+import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
