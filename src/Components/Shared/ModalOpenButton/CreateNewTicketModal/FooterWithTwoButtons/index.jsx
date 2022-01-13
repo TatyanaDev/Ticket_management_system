8ed7ee9334c755ff5f37e14f69e9893becb9c642
karpenko-dev <tetiana.karpenko@rezet.io>
2022-01-13 10:37:54 +0200

    Refactor component FooterWithTwoButtons

--- src/Components/Shared/ModalOpenButton/CreateNewTicketModal/FooterWithTwoButtons/index.jsx
+++ src/Components/Shared/ModalOpenButton/CreateNewTicketModal/FooterWithTwoButtons/index.jsx
@@ -1,13 +1,13 @@
 import { Button, Modal } from "react-bootstrap";
 import style from "./styles.module.css";
 
-export default function FooterWithTwoButtons({ secondaryButtonTitle, primaryButtonTitle, secondaryButtonClick, primaryButtonClick = () => {} }) {
+export default function FooterWithTwoButtons({ secondaryButtonTitle, primaryButtonTitle, secondaryButtonClick, type, primaryButtonClick = () => {} }) {
   return (
     <Modal.Footer className={style.footer}>
       <Button variant='secondary' onClick={secondaryButtonClick} className={style.button_outline}>
         {secondaryButtonTitle}
       </Button>
-      <Button variant='primary' type='submit' className={style.button_primary} onClick={primaryButtonClick}>
+      <Button variant='primary' type={type} className={style.button_primary} onClick={primaryButtonClick}>
         {primaryButtonTitle}
       </Button>
     </Modal.Footer>
